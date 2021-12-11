5fa0bc0dd1decaf58a513c200297afdd611da2f1
confucianzuoyuan <zuoyuantc@126.com>
2021-12-11 11:33:43 +0800

    实现+和-运算符

--- main.c
+++ main.c
@@ -7,8 +7,27 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".global main\n");
   printf("main:\n");
-  printf("  li a0, %d\n", atoi(argv[1]));
+  printf("  li a0, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  addi a0, a0, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  addi a0, a0, -%ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "未识别字符：'%c'\n", *p);
+    return 1;
+  }
   return 0;
 }
