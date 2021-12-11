c7d6138afa171be5104fc9c2af16c0fe620f6c99
confucianzuoyuan <zuoyuantc@126.com>
2021-12-11 18:45:16 +0800

    改进错误信息
    现在zhizhicc可以输入如下错误信息，包含位置信息
    
    $ ./zhizhicc 1+foo
    1+foo
      ^ 预期是数值

--- main.c
+++ main.c
@@ -21,6 +21,9 @@ struct Token {
   int len;        // 标记的长度
 };
 
+// 输入的字符串
+static char *current_input;
+
 // 报错然后退出
 static void error(char *fmt, ...) {
   va_list ap;
@@ -30,6 +33,30 @@ static void error(char *fmt, ...) {
   exit(1);
 }
 
+// 报错并指出错误出现的位置，然后退出
+static void verror_at(char *loc, char *fmt, va_list ap) {
+  int pos = loc - current_input;
+  fprintf(stderr, "%s\n", current_input);
+  // 打印出错位置前面的空格
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+static void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(loc, fmt, ap);
+}
+
+static void error_tok(Token *tok, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(tok->loc, fmt, ap);
+}
+
 // 如果当前标记可以匹配`s`，那么消费掉
 static bool equal(Token *tok, char *op) {
   return memcmp(tok->loc, op, tok->len) == 0 && op[tok->len] == '\0';
@@ -37,13 +64,13 @@ static bool equal(Token *tok, char *op) {
 
 // 确保当前标记是`s`，然后跳过
 static Token *skip(Token *tok, char *s) {
-  if (!equal(tok, s)) error("预期的字符串是：'%s'", s);
+  if (!equal(tok, s)) error_tok(tok, "预期的字符串是：'%s'", s);
   return tok->next;
 }
 
 // 确保当前标记是TK_NUM，然后获取数值
 static int get_number(Token *tok) {
-  if (tok->kind != TK_NUM) error("预期是一个数值");
+  if (tok->kind != TK_NUM) error_tok(tok, "预期是一个数值");
   return tok->val;
 }
 
@@ -57,7 +84,8 @@ static Token *new_token(TokenKind kind, char *start, char *end) {
 }
 
 // 对`p`指针指向的字符串进行词法分析，然后返回新的标记序列。
-static Token *tokenize(char *p) {
+static Token *tokenize(void) {
+  char *p = current_input;
   Token head = {};
   Token *cur = &head;
 
@@ -85,7 +113,7 @@ static Token *tokenize(char *p) {
       continue;
     }
 
-    error("无效的标记");
+    error_at(p, "无效的标记");
   }
 
   cur = cur->next = new_token(TK_EOF, p, p);
@@ -97,7 +125,8 @@ int main(int argc, char **argv) {
     error("%s: 参数数量不正确", argv[0]);
   }
 
-  Token *tok = tokenize(argv[1]);
+  current_input = argv[1];
+  Token *tok = tokenize();
 
   printf(".global main\n");
   printf("main:\n");
@@ -118,5 +147,6 @@ int main(int argc, char **argv) {
     printf("  addi a0, a0, -%d\n", get_number(tok));
     tok = tok->next;
   }
+  
   return 0;
 }
