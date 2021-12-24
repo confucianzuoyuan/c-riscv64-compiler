bccdbd2c20863a74ecc37c48a5548ace272b637a
confucianzuoyuan <zuoyuantc@126.com>
2021-12-24 15:16:48 +0800

    重构，功能无改变

--- codegen.c
+++ codegen.c
@@ -11,6 +11,14 @@ static Obj *current_fn;
 static void gen_expr(Node *node);
 static void gen_stmt(Node *node);
 
+static void println(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vprintf(fmt, ap);
+  va_end(ap);
+  printf("\n");
+}
+
 static int count(void) {
   static int i = 1;
   return i++;
@@ -18,15 +26,15 @@ static int count(void) {
 
 // 将a0寄存器中的内容压栈
 static void push(void) {
-  printf("  addi sp, sp, -8\n");
-  printf("  sd a0, 0(sp)\n");
+  println("  addi sp, sp, -8");
+  println("  sd a0, 0(sp)");
   depth++;
 }
 
 // 将栈顶的内容弹栈到arg寄存器中
 static void pop(char *arg) {
-  printf("  ld %s, 0(sp)\n", arg);
-  printf("  addi sp, sp, 8\n");
+  println("  ld %s, 0(sp)", arg);
+  println("  addi sp, sp, 8");
   depth--;
 }
 
@@ -43,10 +51,10 @@ static void gen_addr(Node *node) {
   switch (node->kind) {
   case ND_VAR:
     if (node->var->is_local) {
-      printf("  addi a0, fp, %d\n", node->var->offset);
+      println("  addi a0, fp, %d", node->var->offset);
     } else {
       // 将全局变量的地址加载到a0寄存器
-      printf("  la a0, %s\n", node->var->name);
+      println("  la a0, %s", node->var->name);
     }
     return;
   case ND_DEREF:
@@ -62,28 +70,28 @@ static void load(Type *ty) {
     return;
   }
   if (ty->size == 1)
-    printf("  lbu a0, 0(a0)\n");
+    println("  lbu a0, 0(a0)");
   else
-    printf("  ld a0, 0(a0)\n");
+    println("  ld a0, 0(a0)");
 }
 
 static void store(Type *ty) {
   pop("a1");
 
   if (ty->size == 1)
-    printf("  sb a0, 0(a1)\n");
+    println("  sb a0, 0(a1)");
   else
-    printf("  sd a0, 0(a1)\n");
+    println("  sd a0, 0(a1)");
 }
 
 static void gen_expr(Node *node) {
   switch (node->kind) {
   case ND_NUM:
-    printf("  li a0, %d\n", node->val);
+    println("  li a0, %d", node->val);
     return;
   case ND_NEG:
     gen_expr(node->lhs);
-    printf("  sub a0, zero, a0\n");
+    println("  sub a0, zero, a0");
     return;
   case ND_VAR:
     gen_addr(node);
@@ -117,7 +125,7 @@ static void gen_expr(Node *node) {
     for (int i = nargs - 1; i >= 0; i--)
       pop(argreg[i]);
 
-    printf("  call %s\n", node->funcname);
+    println("  call %s", node->funcname);
     return;
   }
   }
@@ -132,31 +140,31 @@ static void gen_expr(Node *node) {
 
   switch (node->kind) {
   case ND_ADD:
-    printf("  add a0, a0, a1\n");
+    println("  add a0, a0, a1");
     return;
   case ND_SUB:
-    printf("  sub a0, a1, a0\n");
+    println("  sub a0, a1, a0");
     return;
   case ND_MUL:
-    printf("  mul a0, a0, a1\n");
+    println("  mul a0, a0, a1");
     return;
   case ND_DIV:
-    printf("  div a0, a1, a0\n");
+    println("  div a0, a1, a0");
     return;
   case ND_EQ:
-    printf("  sub a0, a1, a0\n");
-    printf("  seqz a0, a0\n");
+    println("  sub a0, a1, a0");
+    println("  seqz a0, a0");
     return;
   case ND_NE:
-    printf("  sub a0, a1, a0\n");
-    printf("  snez a0, a0\n");
+    println("  sub a0, a1, a0");
+    println("  snez a0, a0");
     return;
   case ND_LT:
-    printf("  slt a0, a1, a0\n");
+    println("  slt a0, a1, a0");
     return;
   case ND_LE:
-    printf("  sgt a0, a1, a0\n");
-    printf("  xori a0, a0, 1\n");
+    println("  sgt a0, a1, a0");
+    println("  xori a0, a0, 1");
     return;
   }
 
@@ -168,30 +176,30 @@ static void gen_stmt(Node *node) {
   case ND_IF: {
     int c = count();
     gen_expr(node->cond);
-    printf("  ble a0, zero, .L.else.%d\n", c);
+    println("  ble a0, zero, .L.else.%d", c);
     gen_stmt(node->then);
-    printf("  j .L.end.%d\n", c);
-    printf(".L.else.%d:\n", c);
+    println("  j .L.end.%d", c);
+    println(".L.else.%d:", c);
     if (node->els)
       gen_stmt(node->els);
-    printf(".L.end.%d:\n", c);
+    println(".L.end.%d:", c);
     return;
   }
   case ND_FOR: {
     int c = count();
     if (node->init)
       gen_stmt(node->init);
-    printf(".L.begin.%d:\n", c);
+    println(".L.begin.%d:", c);
     if (node->cond) {
       gen_expr(node->cond);
-      printf("  ble a0, zero, .L.end.%d\n", c);
+      println("  ble a0, zero, .L.end.%d", c);
     }
     gen_stmt(node->then);
     if (node->inc) {
       gen_expr(node->inc);
     }
-    printf("  j .L.begin.%d\n", c);
-    printf(".L.end.%d:\n", c);
+    println("  j .L.begin.%d", c);
+    println(".L.end.%d:", c);
     return;
   }
   case ND_BLOCK:
@@ -200,7 +208,7 @@ static void gen_stmt(Node *node) {
     return;
   case ND_RETURN:
     gen_expr(node->lhs);
-    printf("  j .L.return.%s\n", current_fn->name);
+    println("  j .L.return.%s", current_fn->name);
     return;
   case ND_EXPR_STMT:
     gen_expr(node->lhs);
@@ -231,15 +239,15 @@ static void emit_data(Obj *prog) {
     if (var->is_function)
       continue;
 
-    printf("  .data\n");
-    printf("  .global %s\n", var->name);
-    printf("%s:\n", var->name);
+    println("  .data");
+    println("  .global %s", var->name);
+    println("%s:", var->name);
     
     if (var->init_data) {
       for (int i = 0; i < var->ty->size; i++)
-        printf("  .byte %d\n", var->init_data[i]);
+        println("  .byte %d", var->init_data[i]);
     } else {
-      printf("  .zero %d\n", var->ty->size);
+      println("  .zero %d", var->ty->size);
     }
   }
 }
@@ -250,36 +258,36 @@ static void emit_text(Obj *prog) {
     if (!fn->is_function)
       continue;
 
-    printf(".global %s\n", fn->name);
-    printf(".text\n");
-    printf("%s:\n", fn->name);
+    println(".global %s", fn->name);
+    println(".text");
+    println("%s:", fn->name);
     current_fn = fn;
 
     // Prologue
-    printf("  addi sp, sp, -16\n");
-    printf("  sd ra, 8(sp)\n"); // 保存返回值地址
-    printf("  sd fp, 0(sp)\n"); // 保存帧指针
-    printf("  mv fp, sp\n");
-    printf("  addi sp, sp, %d\n", -fn->stack_size);
+    println("  addi sp, sp, -16");
+    println("  sd ra, 8(sp)"); // 保存返回值地址
+    println("  sd fp, 0(sp)"); // 保存帧指针
+    println("  mv fp, sp");
+    println("  addi sp, sp, %d", -fn->stack_size);
 
     // 将从寄存器传递过来的参数压栈
     int i = 0;
     for (Obj *var = fn->params; var; var = var->next)
       if (var->ty->size == 1)
-        printf("  sb %s, %d(fp)\n", argreg[i++], var->offset);
+        println("  sb %s, %d(fp)", argreg[i++], var->offset);
       else
-        printf("  sd %s, %d(fp)\n", argreg[i++], var->offset);
+        println("  sd %s, %d(fp)", argreg[i++], var->offset);
 
     gen_stmt(fn->body);
     assert(depth == 0);
 
     // Epilogue
-    printf(".L.return.%s:\n", fn->name);
-    printf("  mv sp, fp\n");
-    printf("  ld ra, 8(sp)\n"); // 恢复返回值地址
-    printf("  ld fp, 0(sp)\n"); // 恢复帧指针
-    printf("  addi sp, sp, 16\n");
-    printf("  ret\n");
+    println(".L.return.%s:", fn->name);
+    println("  mv sp, fp");
+    println("  ld ra, 8(sp)"); // 恢复返回值地址
+    println("  ld fp, 0(sp)"); // 恢复帧指针
+    println("  addi sp, sp, 16");
+    println("  ret");
   }
 }
 
