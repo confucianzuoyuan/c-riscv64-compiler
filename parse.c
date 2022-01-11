f4daba90ff1115122cc0a8a22478878830dfae4a
confucianzuoyuan <zuoyuantc@126.com>
2022-01-11 17:52:14 +0800

    添加前缀运算符++和--

--- parse.c
+++ parse.c
@@ -910,6 +910,7 @@ static Node *cast(Token **rest, Token *tok) {
 }
 
 // unary = ("+" | "-" | "*" | "&") cast
+//       | ("++" | "--") unary
 //       | postfix
 static Node *unary(Token **rest, Token *tok) {
   if (equal(tok, "+"))
@@ -923,6 +924,14 @@ static Node *unary(Token **rest, Token *tok) {
 
   if (equal(tok, "*"))
     return new_unary(ND_DEREF, cast(rest, tok->next), tok);
+
+  // Read ++i as i+=1
+  if (equal(tok, "++"))
+    return to_assign(new_add(unary(rest, tok->next), new_num(1, tok), tok));
+
+  // Read --i as i-=1
+  if (equal(tok, "--"))
+    return to_assign(new_sub(unary(rest, tok->next), new_num(1, tok), tok));
 
   return postfix(rest, tok);
 }
